95b40f32561eadd57d9d0dec56b7fb3252aa4c26
Samer Zqzooq <mrsamerx@icloud.com>
2021-09-05 20:52:49 +0300

    Fixed typing error

--- bookstore.js
+++ bookstore.js
@@ -1,4 +1,12 @@
 // Bookstore Project with Javascript Language
 
-// Book ID, Book Title, Author, Title, Quantity
-Const books = [];
+// Book ID, Book Title, Author, Price, Quantity
+const books = [
+    [1, 2, 3, 4, 5],
+    ["Start with why", "But how do it know", "Clean Code", "Zero to One", "You don't know JS"],
+    ["Simon Sinek", "J. Clark Scott", "Robert Cecil Martin", "Peter Thiel", "Kyle Simpson"],
+    [80.0, 59.9, 50.0, 45.0, 39.9],
+    [13, 22, 5, 12, 9]
+];
+
+console.log(books[1][1]);
